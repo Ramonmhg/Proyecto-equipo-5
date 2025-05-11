94e0b0e4f8a3649328b2e2451e384f7e3cdc95a6
ivanavila86 <ivanavila86@gmail.com>
2025-05-11 12:06:27 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,12 @@
 <?php
-$usuario = 'empleados';  // Cambia según tu usuario Oracle
-$contrasena = '1234';  // Cambia por tu contraseña
-$conexion = oci_connect($usuario, $contrasena, 'localhost/XE');
+$host = "localhost";
+$user = "root"; // o el usuario que tengas en tu phpMyAdmin
+$password = ""; // tu contraseña si tienes
+$database = "proyecto";
+
+$conexion = mysqli_connect($host, $user, $password, $database);
 
 if (!$conexion) {
-    $e = oci_error();
-    die("Error de conexión: " . $e['message']);
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
